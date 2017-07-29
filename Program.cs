154ffdd015a381c9734536734f5ef0a975a92dd0
Justin Kotalik <jukotali@microsoft.com>
2017-07-28 17:02:49 -0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+using Microsoft.AspNetCore.Server.Kestrel.Core;
+
+namespace KestrelLimitTest
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            BuildWebHost(args).Run();
+        }
+
+        public static IWebHost BuildWebHost(string[] args) =>
+            WebHost.CreateDefaultBuilder(args)
+                .UseStartup<Startup>()
+                .UseKestrel(options =>
+                {
+                    // options.Limits.MaxConcurrentConnections = 10;
+                    // options.Limits.MaxConcurrentUpgradedConnections = 10;
+
+                    // Test Default, below default, above default
+                    //options.Limits.MaxRequestBodySize = 3000000; // 2.s86 MB
+                    //options.Limits.MinResponseDataRate = new MinDataRate(100, gracePeriod: TimeSpan.FromSeconds(5));
+                    //options.Limits.MinRequestBodyDataRate = null;
+                })
+                .Build();
+    }
+}
---
+++ Startup.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.AspNetCore.Http.Features;
+
+namespace KestrelLimitTest
+{
+    public class Startup
+    {
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IHostingEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+            app.Run(async (context) =>
+            {
+                var res = context.Features.Get<IHttpMaxRequestBodySizeFeature>();
+                //res.MaxRequestBodySize = 1000;
+                for(int i = 0; i < 1; i++)
+                {
+                    var arr = new byte[20];
+                    context.Request.Body.Read(arr, 0, 20);
+                    await context.Response.WriteAsync("Hello World!");
+                }
+            });
+        }
+    }
+}
